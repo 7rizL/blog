cb3a1ef2eb6f2aea1c993aaa37e57a13ed614398
7rizL <gnusea25@gmail.com>
2021-10-06 22:33:34 +0900

    회원가입 기능 추가

--- src/main/java/com/gnusea/blog/service/UserService.java
+++ src/main/java/com/gnusea/blog/service/UserService.java
@@ -4,6 +4,7 @@ import com.gnusea.blog.dto.SignupRequestDto;
 import com.gnusea.blog.model.User;
 import com.gnusea.blog.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -11,20 +12,24 @@ import java.util.Optional;
 @Service
 public class UserService {
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @Autowired
-    public UserService(UserRepository userRepository) {
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     public void registerUser(SignupRequestDto requestDto) {
         String username = requestDto.getUsername();
-        String password = requestDto.getPassword();
         // 회원 ID 중복 확인
         Optional<User> found = userRepository.findByUsername(username);
         if (found.isPresent()) {
             throw new IllegalArgumentException("중복된 사용자 ID 가 존재합니다.");
         }
+
+        // 패스워드 암호화
+        String password = passwordEncoder.encode(requestDto.getPassword());
 
         User user = new User(username, password);
         userRepository.save(user);
