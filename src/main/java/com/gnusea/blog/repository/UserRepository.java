d4c1d9a60ac54b26ff80af597e10c635832ae9e4
7rizL <gnusea25@gmail.com>
2021-10-06 15:40:31 +0900

    회원가입 페이지 구현

---
+++ src/main/java/com/gnusea/blog/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.gnusea.blog.repository;
+
+import com.gnusea.blog.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+}
---
+++ src/main/java/com/gnusea/blog/service/UserService.java
@@ -0,0 +1,32 @@
+package com.gnusea.blog.service;
+
+import com.gnusea.blog.dto.SignupRequestDto;
+import com.gnusea.blog.model.User;
+import com.gnusea.blog.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public void registerUser(SignupRequestDto requestDto) {
+        String username = requestDto.getUsername();
+        String password = requestDto.getPassword();
+        // 회원 ID 중복 확인
+        Optional<User> found = userRepository.findByUsername(username);
+        if (found.isPresent()) {
+            throw new IllegalArgumentException("중복된 사용자 ID 가 존재합니다.");
+        }
+
+        User user = new User(username, password);
+        userRepository.save(user);
+    }
+}
