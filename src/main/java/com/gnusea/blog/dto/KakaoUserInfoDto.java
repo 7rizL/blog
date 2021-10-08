4befac0399e864ae08dfcd795d7a8e887a2f3ce6
7rizL <gnusea25@gmail.com>
2021-10-08 12:07:11 +0900

    카카오 로그인 추가

---
+++ src/main/java/com/gnusea/blog/dto/KakaoUserInfoDto.java
@@ -0,0 +1,11 @@
+package com.gnusea.blog.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class KakaoUserInfoDto {
+    private Long id;
+    private String nickname;
+}
